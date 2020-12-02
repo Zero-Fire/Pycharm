a6f3fdfa7eb659096af5eec2beb9ffd50644e30d
Zreo-Fire <992106950@qq.com>
2020-12-02 10:13:40 +0800

    练习

--- self_pratice/1201.py
+++ self_pratice/1201.py
@@ -5,10 +5,13 @@ class person:
     gen : str = "男"
     __money : float = 20000
     def __init__(self,name,age,gen,money):
+        print('这是一个构造函数')
         self.name = name
         self.age = age
         self.gen=gen
         self.__money=money
+    def get_money(self):
+        return self.__money
     def eat (self):
         print(f"{self.name} is eating")
         return  True
@@ -18,7 +21,29 @@ class person:
 
     def jump(self):
         print(f"{self.name} is jumping")
-zs=person('张三',20,'男',2000)
-print(zs.name)
-zs.eat()
-zs.jump()
+
+class funnyman(person):
+    skill : str = ''
+
+    def __init__(self,skill,name,age,gen,money):
+        self.skill=skill
+        super().__init__(name,age,gen,money)
+    def fun(self):
+        print(f'{self.name} is funny')
+
+st = funnyman('haha','st',20,'男',2000)
+print(st.name)
+st.fun()
+
+# zs=person('张三',20,'男',2000)
+# print(zs.name)
+# print(zs.age)
+# print(zs.gen)
+# zs.eat()
+# print(zs.get_money())
+# # print(dir(zs))
+# zs.jump()
+# zs.name='王五'
+# person.name="default"
+# print(person.name)
+# print(zs.name)
