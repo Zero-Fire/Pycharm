4b2756db7c7bb6d9c84941467e6b7cdbb03d8380
Zreo-Fire <992106950@qq.com>
2020-12-03 21:34:08 +0800

    练习

--- testing/test_code.py
+++ testing/test_code.py
@@ -1,8 +1,30 @@
 # -*- coding: utf-8 -*-
+import yaml
 import pytest
-from Calcu.demo1 import Calculator
+from Calcu.calc_code import Calculator
+with open ('./data/calc_data.yaml') as f:
+    add_data=yaml.safe_load(f)['add']
+    data=add_data['data']
 
-def test_add():
-    a=Calculator()
-    result = a.add(1,1)
-    assert result==2
+class TestCalculator:
+
+    @pytest.mark.parametrize(
+        'a,b,expect',data,
+        ids=['int']
+    )
+    def setup_class(self):
+        print('计算开始')
+
+    def teardown_class(self):
+        print('计算结束')
+
+    def test_add(self,a,b,expect):
+        #实例化计算器类
+        calc= Calculator()
+        result=calc.add(a,b)
+        #判断结果是否为浮点类型，是就保留2位小数
+        if isinstance(result,float):
+            result=round(result,2)
+        assert result == expect
+
+
