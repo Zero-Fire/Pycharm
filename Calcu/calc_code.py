34db2a2cd915a5cef76c9b3f91c4569518df2992
Zreo-Fire <992106950@qq.com>
2020-12-06 14:55:57 +0800

    练习

--- Calcu/calc_code.py
+++ Calcu/calc_code.py
@@ -2,13 +2,24 @@
 class Calculator:
 
     def add(self, a, b):
+        if (type(a)!= int and type(a)!=float) or (type(b) != int and type(b)!=float):
+            return "请输入数字"
         return a + b
-
+    def sub(self,a,b):
+        if (type(a)!=int and type (a)!=float) or (type(b)!=int and type(b)!=float):
+            return "请输入数字"
+        return a-b
+    def mul(self,a,b):
+        if (type(a)!=int and type(a)!=float) or (type(b)!=int and type(b)!=float):
+            return "请输入数字"
+        return a*b
     def div(self, a, b):
-        if b == 0:
+        if (type(a)!= int and type(a)!=float) or (type(b)!= int and type(b)!=float):
+            return "请输入数字"
+        elif b == 0:
             return 'ZeroDivisionError'
         else:
             return a / b
 
-# a=Calculator()
-# print(a.div(0,1))
+a=Calculator()
+print(a.add('\n',0.2))
--- testing/test_code.py
+++ testing/test_code.py
@@ -10,13 +10,16 @@ def setup_module():
 def teardown_module():
     print('计算结束')
 
-with open ('./data/calc_data.yaml') as f:
+with open ('./data/calc_data.yaml',encoding='utf-8') as f:
     add_data=yaml.safe_load(f)['add']
     data=add_data['data']
     key=add_data['key']
     div=add_data['div']
     keys=add_data['keys']
-
+    sub_data=add_data['sub_data']
+    sub_key=add_data['sub_key']
+    mul_data=add_data['mul_data']
+    mul_key=add_data['mul_key']
 
 class TestCalculator:
 
@@ -25,7 +28,6 @@ class TestCalculator:
         'a,b,expect', data,
         ids=key
     )
-
     def test_add(self,a,b,expect):
         #实例化计算器类
         calc = Calculator()
@@ -35,8 +37,6 @@ class TestCalculator:
             result=round(result,2)
         assert result == expect
 
-class TestCalculator_1:
-
     @pytest.mark.parametrize('a,b,expect',div,ids=keys)
 
     def test_div(self,a,b,expect):
@@ -44,6 +44,23 @@ class TestCalculator_1:
         result = calc.div (a,b)
         if isinstance(result,float):
             result=round(result,2)
+        assert result == expect
+
+    @pytest.mark.parametrize('a,b,expect',sub_data,ids=sub_key)
+
+    def test_sub(self,a,b,expect):
+        calc = Calculator()
+        result=calc.sub(a,b)
+        if isinstance(result,float):
+            result=round(result,4)
+        assert result == expect
+
+    @pytest.mark.parametrize('a,b,expect',mul_data,ids=mul_key)
+    def test_mul(self,a,b,expect):
+        calc=Calculator()
+        result = calc.mul(a,b)
+        if isinstance(result,float):
+            result = round(result,4)
         assert result == expect
 
 
